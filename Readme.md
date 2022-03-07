498f41c5a330989d9852eb5958838f3325ea2850
Le Nguyen <lenguyen1612@gmail.com>
2022-03-07 23:30:00 +0700

    completed the test case

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Required
+
+- eclipse (2020)
+- TestNG plugin
+- Java 7
+- Chrome browser version 99.0.4844.51
+- OS: Windowns 10
+
+# Build project
+- Open eclipse and import maven project
+- Update maven project
--- testcases/openweathermap/search/testSearchWeather.java
+++ testcases/openweathermap/search/testSearchWeather.java
@@ -1,68 +1,73 @@
 package openweathermap.search;
 
+import static org.testng.Assert.assertTrue;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class testSearchWeather {
-	
+
 	WebDriver driver;
-	
+
 	@Test
 	public void searchWeatherInYourCity() throws InterruptedException {
 		String path = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", path+"/drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://openweathermap.org/");
-		
+
 		WebElement pageContainer = driver.findElement(By.cssSelector("div.page-container"));
 		WebElement searchText = pageContainer.findElement(By.cssSelector("div.search-container input"));
-		
-		
+
 		searchText.sendKeys("Ha Noi");
-		Thread.sleep(500);
+		Thread.sleep(5000);
 		WebElement buttonSearch = pageContainer.findElement(By.cssSelector("button"));
-		
+
 		buttonSearch.click();
-		Thread.sleep(500);
-		
+		Thread.sleep(5000);
+
 		WebElement selectResults = pageContainer.findElement(By.cssSelector("div.search-container ul"));
 		selectResults.click();
-	}
-	
-	public boolean isLoadedSuccess(WebDriver driver) {
-		WebDriverWait explicitWait = new WebDriverWait(driver, 10);
-		final JavascriptExecutor jsExecutor = (JavascriptExecutor)driver;
-		ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
-			public Boolean apply(WebDriver driver) {
-				try{
-					return ((Long)jsExecutor.executeScript("return jQuery.active")==0);
-				}catch(Exception e) {
-					return true;
-				}
-			}
-		};
+
+		Thread.sleep(5000);
+		WebElement widgetWeather = driver.findElement(By.cssSelector("div[class='grid-container grid-4-5']"));
+		WebElement cityName = widgetWeather.findElement(By.cssSelector("div h2"));
+		System.out.println(cityName.getText() );
+		assertTrue(cityName.getText().contains("Ha Noi"), "Should be displayed correctly city name");
+
+		WebElement temp = widgetWeather.findElement(By.cssSelector("div.current-temp span"));
+		String tempNumber = "";
 		
-//		ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
-//			public Boolean apply(WebDriver driver) {
-//				try{
-//					return ((Long)jsExecutor.executeScript("return jQuery.active")==0);
-//				}catch(Exception e) {
-//					return true;
-//				}
-//			}
-//		};
+		System.out.println("temp:" + temp.getText() ); 
 		
-		return false;
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher(temp.getText());
+		while (m.find()) {
+			tempNumber = m.group();
+		}
+
+		assertTrue(isNumeric(tempNumber), "");
 	}
-	
-	
+
+	private static boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
 	@AfterTest
 	public void afterTest() {
 		driver.quit();
